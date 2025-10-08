1bb10a4cd3786482c3623f90562407a65a45c822
Vlocodeu <developer@vlocodeu.com>
2025-10-08 21:44:52 +0530

    Update api.ts

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: '/', // Vite proxy handles /api -> backend
+  baseURL:  import.meta.env.VITE_API_URL,
   timeout: 10000,
   withCredentials: true,
 });
